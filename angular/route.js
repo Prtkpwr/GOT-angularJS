06ba95ac3972ec52b2cbf67018738d1c91158826
Prtkpwr <Pratik010pawar@gmail.com>
2017-12-06 14:35:21 +0530

    detailed views added

--- angular/route.js
+++ angular/route.js
@@ -5,6 +5,21 @@ myApp.config(["$routeProvider",function($routeProvider){
               controller: "mainController",
               controllerAs : "mainCtrl"
             })
+    .when("/books/:id1",{
+              templateUrl : "view/book-detail.html",
+              controller : "booksController",
+              controllerAs : "bookCtrl"
+            })
+    .when("/characters/:id2",{
+              templateUrl : "view/char-detail.html",
+              controller : "charController",
+              controllerAs : "charCtrl"
+            })
+.when("/houses/:id3",{
+              templateUrl : "view/house-detail.html",
+               controller : "houseController",
+              controllerAs : "hCtrl"
+            })
 
 
 			.otherwise(
